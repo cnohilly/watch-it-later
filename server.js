402caa94d93123c1ad219f68c7d0f3f1a6aacbe4
cnohilly <christopher.nohilly@snhu.edu>
2022-08-09 19:56:48 -0400

    more starter files to start server

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+// sets the port to the env variable or defaults to 3001
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'watchlist secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port ' + PORT));
+});

f77ad9b74163ddceec9fd9811ee3eb7a46412154
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-08-09 20:22:47 -0400

    added user, vote and comment file structure

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User, Comment, Vote } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});

9aee31d4e7c0c10eaaf1c47cb5476cadeae208c2
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-08-11 20:36:32 -0400

    added create user and search one user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,6 +13,54 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where:{
+      id: req.body.id
+    },
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.post("/", (req, res) => {
+  
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
@@ -58,4 +106,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
->>>>>>> feature/endpoints

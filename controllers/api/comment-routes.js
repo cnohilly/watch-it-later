63744b0d648c3fa9c713adbd3d5452c2ead02da5
ctnohilly <ctnohilly@gmail.com>
2022-08-15 13:27:47 -0400

    adjusted routes and added files to allow users to post comments on content pages

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -6,74 +6,74 @@ const withAuth = require('../../utils/auth');
 
 // get all comments
 router.get('/', (req, res) => {
-    Comment.findAll()
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // get a comment by id
 router.get('/:id', (req, res) => {
-    Comment.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [
-        {
-          model: User,
-          attributes: ['id']
-        }
-      ]
+  Comment.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['id']
+      }
+    ]
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id' });
+        return;
+      }
+      res.json(dbCommentData);
     })
-      .then(dbCommentData => {
-        if (!dbCommentData) {
-          res.status(404).json({ message: 'No comment found with this id'}); 
-          return; 
-        }
-        res.json(dbCommentData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // post a new comment
 router.post('/', withAuth, (req, res) => {
-    // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
-    Comment.create({
-        id: req.body.id,
-        comment_text: req.body.comment_text,
-        user_id: req.session.user_id,
-        movie_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-  });
+  // expects => {comment_text: "This is the comment", user_id: 1, content_id: 200, content_type:'movie'}
+  Comment.create({
+    comment_text: req.body.text,
+    user_id: req.session.user_id,
+    content_id: req.body.id,
+    content_type: req.body.type
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
-  // delete comment
-  router.delete('/:id', withAuth, (req, res) => {
-    Comment.destroy({
-      where: {
-        id: req.params.id
+// delete comment
+router.delete('/:id', withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
       }
+      res.json(dbCommentData);
     })
-      .then(dbCommentData => {
-        if (!dbCommentData) {
-          res.status(404).json({ message: 'No comment found with this id!' });
-          return;
-        }
-        res.json(dbCommentData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- public/javascript/watchlist-remove.js
+++ public/javascript/watchlist-remove.js
@@ -4,32 +4,19 @@ async function removeFromWatchlist(event) {
     const loc = window.location.toString().split('/');
     const type = this.getAttribute('data-content-type');
     const contentId = this.getAttribute('data-content-id');
-    const response = await fetch('/api/watchlist/' + type + '/' + contentId, {
-        method: 'DELETE',
-    });
+    try {
+        const response = await fetch('/api/watchlist/' + type + '/' + contentId, {
+            method: 'DELETE',
+        });
 
-    if (response.ok) {
-        $(this).closest('.content-card').slideUp(250, function () {
-            $(this).remove();
-        })
+        if (response.ok) {
+            $(this).closest('.content-card').slideUp(250, function () {
+                $(this).remove();
+            })
+        }
+    } catch (err) {
+        console.log(err);
     }
-    // const response = await fetch('/api/watchlist', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         type,
-    //         id,
-    //         title,
-    //         poster,
-    //         year
-    //     }),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // });
-
-    // if (response.ok) {
-    //     document.location.reload();
-    // }
 }
 
 $('.section').on('click', '.remove-button', removeFromWatchlist);

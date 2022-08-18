06eb3c9331ca1cb814da2e90626b97882a4cda45
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-17 22:44:47 -0400

    cleaned search.js code

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,25 +1,9 @@
-// API information
-const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8da71b56dc2b287e98f9875203b81d2c&page=1'
-
-const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
-
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=8da71b56dc2b287e98f9875203b81d2c&query=';
-
-//Selecting Elements
-const main = document.getElementById('main');
-
-const form = document.getElementById('form');
-
-//Search function
+//Searching for content 
 function searchContent(query, type) {
   var apiUrl = 'https://api.themoviedb.org/3/search/' + type + '?api_key=' + process.env.TMDB_KEY + '&query=' + query;
   return axios(apiUrl)
 }
 
-
-// call the showMovies function that requests the movie data from the Api using fetch.
-//Then it puts those data in the main HTML tag by creating elments for those data.
-// showMovies(apiUrl);
 
 function search(event) {
   event.preventDefault();
